153100cb49a4a3e698d77e2ecfe7b4ae27fc19bc
Fabrice Triboix <ftriboix@gmail.com>
2017-05-04 16:25:45 +0100

    skald: const-correctness

--- skald/src/skald.c
+++ skald/src/skald.c
@@ -324,10 +324,12 @@ static void skaldGroupRemoveSubscriber(const char* group,
 
 /** Send the given message to all subcribers of the given group
  *
+ * The ownership of `msg` stays with the caller.
+ *
  * @param group [in] Name of the group; must not be NULL
  * @param msg   [in] Message to send; must not be NULL
  */
-static void skaldGroupDispatch(const char* group, SkalMsg* msg);
+static void skaldGroupDispatch(const char* group, const SkalMsg* msg);
 
 
 
@@ -1051,7 +1053,7 @@ void skaldGroupRemoveSubscriber(const char* group,
 }
 
 
-static void skaldGroupDispatch(const char* group, SkalMsg* msg)
+static void skaldGroupDispatch(const char* group, const SkalMsg* msg)
 {
     SKALASSERT(SkalIsAsciiString(group));
     SKALASSERT(msg != NULL);
