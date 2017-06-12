946231a298bc4af88be1aff7fed93568bda259c6
Fabrice Triboix <ftriboix@gmail.com>
2017-06-12 08:40:12 +0100

    skal-net: Explicitely handle EPIPE when sending
    This is to inform the upper layer that the peer has closed the
    connection while we were trying to send data to it. If we try to send a
    lot of data, there is a significant chance that the send will fail with
    that error number if the peer closes its socket.

--- lib/net/src/skal-net.c
+++ lib/net/src/skal-net.c
@@ -1526,6 +1526,10 @@ static SkalNetSendResult skalNetSendPacket(SkalNet* net, int sockid,
                 result = SKAL_NET_SEND_TOO_BIG;
                 done = true;
                 break;
+            case EPIPE :
+                result = SKAL_NET_SEND_RESET;
+                done = true;
+                break;
             default :
                 {
                     // Unexpected error
