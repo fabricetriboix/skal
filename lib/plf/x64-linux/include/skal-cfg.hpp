5cab6e65d7cd18cf282db80cbd68ce1d1421f820
Fabrice Triboix <ftriboix@gmail.com>
2017-07-19 06:54:43 +0100

    Move skal-cfg.hpp to C++

--- lib/plf/x64-linux/include/skal-cfg.hpp
+++ lib/plf/x64-linux/include/skal-cfg.hpp
@@ -14,12 +14,14 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef SKAL_CFG_h_
-#define SKAL_CFG_h_
+#pragma once
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include <cstdint>
+#include <string>
+#include <boost/chrono.hpp>
+
+
+namespace skal {
 
 
 /** SKAL compile-time configuration
@@ -32,87 +34,57 @@ extern "C" {
  */
 
 
-
 /** Default TTL */
-#define SKAL_DEFAULT_TTL 4
+const int8_t DEFAULT_TTL(4);
 
 
 /** Default XOFF timeout (how long to wait before retrying to send) */
-#define SKAL_DEFAULT_XOFF_TIMEOUT_us 50000
+const boost::chrono::milliseconds DEFAULT_XOFF_TIMEOUT(50);
 
 
 /** Default queue threshold
  *
  * This is also the value that is used by the skal-master threads.
  */
-#define SKAL_DEFAULT_QUEUE_THRESHOLD 100
+const int DEFAULT_QUEUE_THRESHOLD(100);
 
 
-/** Default skal-net style URL to connect to skald */
-#define SKAL_DEFAULT_SKALD_URL "unix:///tmp/skald.sock"
-
-
-/** Default backlog value for server sockets */
-#define SKAL_NET_DEFAULT_BACKLOG 20
+/** Default URL to connect to skald */
+const std::string DEFAULT_SKALD_URL("unix:///tmp/skald.sock");
 
 
 /** Default timeout value for connectionless sockets */
-#define SKAL_NET_DEFAULT_TIMEOUT_us (10 * 1000 * 1000)
-
-
-/** Socket polling timeout
- *
- * Please note this affects only how often connectionless sockets are checked
- * for termination.
- */
-#define SKAL_NET_POLL_TIMEOUT_us (10 * 1000)
+const boost::chrono::milliseconds DEFAULT_CNXLESS_TIMEOUT(10 * 1000);
 
 
 /** Default socket buffer size, in bytes */
-#define SKAL_NET_DEFAULT_BUFSIZE_B (128 * 1024)
+const int DEFAULT_BUFSIZE_B(128 * 1024);
 
 
 /** Minimum socket buffer size, in bytes */
-#define SKAL_NET_MIN_BUFSIZE_B 2048
+const int MIN_BUFSIZE_B(2048);
 
 
 /** Maximum socket buffer size, in bytes */
-#define SKAL_NET_MAX_BUFSIZE_B 212992
-
-
-/** Permissions for shared memory files
- *
- * These are set to read/write for both the creator of the shared memory file
- * and the group, but no access to others.
- */
-#define SKAL_SHM_PERM (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP)
+const int MAX_BUFSIZE_B(212992);
 
 
 /** Default MTU: standard ethernet frame, less a few bytes for VLAN id, etc. */
-#define SKAL_PROTO_DEFAULT_MTU_B 1472
+const int DEFAULT_MTU_B(1472);
 
 
 /** Default retransmit timeout */
-#define SKAL_PROTO_DEFAULT_RETRANSMIT_TIMEOUT_ms 5
+const boost::chrono::milliseconds DEFAULT_RETRANSMIT_TIMEOUT(5);
 
 
 /** Default send queue size */
-#define SKAL_PROTO_DEFAULT_SEND_QUEUE_SIZE 5
+const int DEFAULT_SEND_QUEUE_SIZE(5);
 
 
 /** Default output bitrate (50Mbps) */
-#define SKAL_PROTO_DEFAULT_BITRATE_bps 50000000
-
-
-/** Default output burst rate (10 packets at default MTU size) */
-#define SKAL_PROTO_DEFAULT_BURST_RATE_bsp (10 * SKAL_PROTO_DEFAULT_MTU_B * 8)
-
+const int DEFAULT_BITRATE_bps(50 * 1000 * 1000);
 
 
 /* @} */
 
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* SKAL_CFG_h_ */
+} // namespace skal
