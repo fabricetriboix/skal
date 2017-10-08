03e2c1391e063c2af4f15e62a42681443ea4cf19
Fabrice Triboix <ftriboix@gmail.com>
2017-10-08 15:13:56 +0100

    Add skal-alarm and skal-thread-specific

---
+++ lib/skal/include/detail/skal-thread-specific.hpp
@@ -0,0 +1,17 @@
+/* Copyright Fabrice Triboix - Please read the LICENSE file */
+
+#pragma once
+
+#include "skal-cfg.hpp"
+#include <string>
+
+namespace skal {
+
+struct thread_specific_t
+{
+    std::string name;
+};
+
+thread_specific_t& thread_specific();
+
+} // namespace skal
