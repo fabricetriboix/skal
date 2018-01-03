83682f0c6243827506723404f9a520b74b27cd9a
Fabrice Triboix <ftriboix@incise.co>
2018-01-03 19:18:28 +0000

    Fix --verbose parsing in skald

--- src/skald/main.cpp
+++ src/skald/main.cpp
@@ -52,9 +52,11 @@ void handle_signal(int signum)
 bool parse_args(int& argc, char**& argv, parameters_t& params)
 {
     po::options_description desc("Run the skald daemon");
+    std::string verbose;
     desc.add_options()
         ("help,h", "Print usage message")
-        ("verbose,v", "Be more verbose (can be cumulated)")
+        ("verbose,v", po::value(&verbose)->implicit_value(""),
+            "Be more verbose (can be cumulated)")
         ("domain,d", po::value<std::string>(), "Set the skald domain")
         ("local,l", po::value<std::string>(), "Local URL to listen to");
     po::variables_map vars;
@@ -71,12 +73,13 @@ bool parse_args(int& argc, char**& argv, parameters_t& params)
     if (vars.count("local")) {
         params.local_url = vars["local"].as<std::string>();
     }
-    if (vars.count("verbose") > 0) {
-        if (vars.count("verbose") > 1) {
-            skal::log::minimum_level(skal::log::level_t::debug);
-        } else {
-            skal::log::minimum_level(skal::log::level_t::info);
-        }
+    if (vars.count("verbose")) {
+        verbose += "v";
+    }
+    if (verbose.size() > 1) {
+        skal::log::minimum_level(skal::log::level_t::debug);
+    } else if (verbose.size() > 0) {
+        skal::log::minimum_level(skal::log::level_t::info);
     }
     if (params.domain.empty()) {
         params.domain = "default-domain";
@@ -86,11 +89,13 @@ bool parse_args(int& argc, char**& argv, parameters_t& params)
 
 } // unnamed namespace
 
-extern "C" int main(int argc, char** argv)
+} // namespace skald
+
+int main(int argc, char** argv)
 {
     struct sigaction sa;
     std::memset(&sa, 0, sizeof(sa));
-    sa.sa_handler = handle_signal;
+    sa.sa_handler = skald::handle_signal;
     int ret = ::sigaction(SIGINT, &sa, nullptr);
     if (ret < 0) {
         skal_log(error) << "sigaction(SIGINT) failed: "
@@ -104,20 +109,18 @@ extern "C" int main(int argc, char** argv)
         return 1;
     }
 
-    parameters_t params;
-    if (!parse_args(argc, argv, params)) {
+    skald::parameters_t params;
+    if (!skald::parse_args(argc, argv, params)) {
         return 1;
     }
 
-    init(params);
+    skald::init(params);
 
-    g_state = state_t::running;
-    while (g_state == state_t::running) {
+    skald::g_state = skald::state_t::running;
+    while (skald::g_state == skald::state_t::running) {
         ::pause();
     }
 
-    terminate();
+    skald::terminate();
     return 0;
 }
-
-} // namespace skald
